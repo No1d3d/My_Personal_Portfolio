bbb8100d3ba450c7ab9999a24fb879063b489875
No1d3d <85016065+No1d3d@users.noreply.github.com>
2024-05-03 11:52:22 +0300

    Delete My version of some of STL containers/src directory

--- My version of some of STL containers/src/tests/tests.h
+++
@@ -1,9 +0,0 @@
-#ifndef S21_CPP_CONTAINERS_TESTS_H_
-#define S21_CPP_CONTAINERS_TESTS_H_
-
-#include <gtest/gtest.h>
-
-#include "../s21_containers.h"
-#include "../s21_containersplus.h"
-
-#endif  // S21_CPP_CONTAINERS_TESTS_H_
