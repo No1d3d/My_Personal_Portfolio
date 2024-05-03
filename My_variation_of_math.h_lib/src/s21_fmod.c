1f017a2ccf6290af1ac454afb7c51a177c1e5108
No1d3d <85016065+No1d3d@users.noreply.github.com>
2024-05-03 11:52:00 +0300

    Delete My_variation_of_math.h_lib/src directory

--- My_variation_of_math.h_lib/src/s21_fmod.c
+++
@@ -1,9 +0,0 @@
-#include "s21_math.h"
-
-long double s21_fmod(double x, double y) {
-  if (s21_isinf(x) || y == 0 || s21_isnan(y))
-    x = S21_NAN;
-  else if (!s21_isinf(y))
-    x = x - (long long)(x / y) * y;
-  return x;
-}
